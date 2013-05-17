f6ab588257179e54965b55e5b430260280037a8b
Guilherme Stiebler <guilhermemst@gmail.com>
2013-05-17 17:38:32 -0300

    ImageRGB

--- cpp/ImageRGB.cpp
+++ cpp/ImageRGB.cpp
@@ -7,16 +7,6 @@ ImageRGB::ImageRGB(uchar *data, int width, int height) :
 {
 }
 
-void ImageRGB::setRGB( int i, int j, uchar r, uchar g, uchar b )
-{
-	int lineOffset = j * _width * 3;
-	int totalOffset = lineOffset + i * 3;
-	uchar *p = &_data[totalOffset];
-	*p++ = r;
-	*p++ = g;
-	*p = b;
-}
-
 int ImageRGB::getWidth()
 {
     return _width;
@@ -27,3 +17,49 @@ int ImageRGB::getHeight()
     return _height;
 }
 
+int ImageRGB::getOffset( int i, int j )
+{
+	int lineOffset = j * _width * 3;
+	return lineOffset + i * 3;
+}
+
+void ImageRGB::setRGB( int i, int j, uchar r, uchar g, uchar b )
+{
+	uchar *p = &_data[getOffset( i, j )];
+	*p++ = r;
+	*p++ = g;
+	*p = b;
+}
+
+
+
+void ImageRGB::setR( int i, int j, uchar r )
+{
+	_data[getOffset( i, j )] = r;
+}
+
+void ImageRGB::setG( int i, int j, uchar g )
+{
+	_data[getOffset( i, j ) + 1] = g;
+}
+
+void ImageRGB::setB( int i, int j, uchar b )
+{
+	_data[getOffset( i, j ) + 2] = b;
+}
+
+
+uchar ImageRGB::getR( int i, int j )
+{
+	return _data[getOffset( i, j )];
+}
+
+uchar ImageRGB::getG( int i, int j )
+{
+	return _data[getOffset( i, j ) + 1];
+}
+
+uchar ImageRGB::getB( int i, int j )
+{
+	return _data[getOffset( i, j ) + 2];
+}
--- cpp/ImageRGB.h
+++ cpp/ImageRGB.h
@@ -8,14 +8,24 @@ class ImageRGB
 {
 public:
     ImageRGB(uchar *data, int width, int height);
-    void setRGB( int i, int j, uchar r, uchar g, uchar b );
     int getWidth();
     int getHeight();
+
+    void setRGB( int i, int j, uchar r, uchar g, uchar b );
+    void setR( int i, int j, uchar r );
+    void setG( int i, int j, uchar g );
+    void setB( int i, int j, uchar b );
+
+    uchar getR( int i, int j );
+    uchar getG( int i, int j );
+    uchar getB( int i, int j );
     
 protected:
     uchar *_data;
     int _width;
     int _height;
+
+	int getOffset( int i, int j);
 };
 
 #endif
