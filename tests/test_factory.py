7ffef1aea92233b6914a8aa193d45608508821ef
Guilherme Stiebler <guilhermemst@gmail.com>
2013-06-29 17:54:40 -0300

    testing main presenter

---
+++ tests/test_factory.py
@@ -0,0 +1,30 @@
+
+import sys
+sys.path.append('../python')
+
+from mock import MagicMock
+
+class DummyTestClass:
+    pass
+
+class TestWindowFactory:
+
+    def new_main_window( self, param ):
+        self.main_window = MagicMock() 
+        return self.main_window
+        
+    def new_func_window( self, function_xml, outputWindow ):
+        self.func_window = MagicMock() 
+        return self.func_window
+    
+    def new_output_window( self ):
+        self.output_window = MagicMock() 
+        return self.output_window
+        
+    def new_capture_window( self, outputWindow ):
+        self.capture_window = MagicMock() 
+        return self.capture_window
+    
+    def new_batch_window( self ):
+        self.batch_window = MagicMock() 
+        return self.batch_window
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,37 @@ import unittest
 import sys
 sys.path.append('../python')
 import main_presenter
+from class_factory import Factory
+import test_factory
 
 class TestMainPresenter(unittest.TestCase):
 
     def setUp(self):
-        presenter = main_presenter.MainPresenter()
+        Factory.default_factory = test_factory.TestWindowFactory()
+        self.presenter = main_presenter.MainPresenter()
 
-    def test_basic(self):
-        presenter = main_presenter.MainPresenter()
+    def test_constructor(self):
+        assert Factory.default_factory.main_window 
+        assert Factory.default_factory.output_window 
         
+        self.assertEqual(5, Factory.default_factory.main_window.add_custom_function.call_count)
+        
+    def test_load_image(self):
+        self.presenter.load_image( "test" )
+        self.assertEqual(2, Factory.default_factory.output_window.set_src_image.call_count)
+        
+    def test_file_loaded(self):
+        self.presenter.file_loaded( "test" )
+        self.assertEqual(2, Factory.default_factory.output_window.set_src_image.call_count)
+        
+    def test_custom_func_button_clicked(self):
+        self.presenter.custom_func_button_clicked( 2 )
+        assert Factory.default_factory.func_window 
+        
+    def test_capture_window_button_clicked(self):
+        self.presenter.capture_window_button_clicked()
+        assert Factory.default_factory.capture_window 
+        
+    def test_batch_window_button_clicked(self):
+        self.presenter.batch_window_button_clicked()
+        assert Factory.default_factory.batch_window 
